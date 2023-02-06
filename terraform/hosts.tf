cca63b2d4cfd23cf855b5069892e1e345e0c4d44
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2023-02-07 00:23:33 +0100

    Añadir .localdomain al fichero de hosts

--- terraform/hosts.tf
+++ terraform/hosts.tf
@@ -1,6 +1,6 @@
 locals {
   hosts = join("\n", [
-    "127.0.0.1 localhost",
+    "127.0.0.1 localhost localhost.localdomain",
     join( " ", [local.master.private_ip, opennebula_virtual_machine.master.name]),
     join("\n", flatten([
       for i in opennebula_virtual_machine.nodes[*] : join(" ", [i.nic[0].computed_ip, i.name])
