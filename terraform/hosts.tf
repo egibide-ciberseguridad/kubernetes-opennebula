a230febf5b49de834d30db38dd1b35847402011c
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2023-02-07 00:23:53 +0100

    Utilizar un mirror local de Ubuntu en vez del oficial

--- terraform/hosts.tf
+++ terraform/hosts.tf
@@ -1,6 +1,7 @@
 locals {
   hosts = join("\n", [
     "127.0.0.1 localhost localhost.localdomain",
+    "172.20.227.241 ubuntu.ciber-egibide.eu",
     join( " ", [local.master.private_ip, opennebula_virtual_machine.master.name]),
     join("\n", flatten([
       for i in opennebula_virtual_machine.nodes[*] : join(" ", [i.nic[0].computed_ip, i.name])
