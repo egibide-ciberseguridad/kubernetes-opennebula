83d79e768b05d6ef952a594d531a13d710631870
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2023-02-28 03:12:09 +0100

    Instalar el gestor de volúmenes persistente Rook

---
+++ terraform/rook.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "ansible_rook" {
+  depends_on = [
+    null_resource.ansible_nodes_kubernetes
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      ANSIBLE_HOST_KEY_CHECKING=False \
+      ansible-playbook \
+        -i "${local.master.connection_ip}," \
+        /ansible/rook-playbook.yml
+    EOT
+  }
+}
