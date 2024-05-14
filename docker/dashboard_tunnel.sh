7af047019f0028dc438197651e810ff16b983b71
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-05-15 01:55:38 +0200

    Usar el bastion host para todas las acciones

--- docker/dashboard_tunnel.sh
+++ docker/dashboard_tunnel.sh
@@ -3,15 +3,18 @@
 TIPO_NODO=$(echo "$1" | cut -d "-" -f 2)
 NUM_NODO=$(echo "$1" | cut -d "-" -f 3)
 
+HAPROXY_IP=$(terraform -chdir=/terraform output --json haproxy | jq -r .connection_ip)
+
 case $TIPO_NODO in
-master) DIRECCION=$(terraform -chdir=/terraform output --json master | jq -r .connection_ip) ;;
-node) DIRECCION=$(terraform -chdir=/terraform output --json nodes | jq -r .[$NUM_NODO].connection_ip) ;;
-haproxy) DIRECCION=$(terraform -chdir=/terraform output --json haproxy | jq -r .connection_ip) ;;
+master) DIRECCION=$(terraform -chdir=/terraform output --json master | jq -r .name) ;;
+node) DIRECCION=$(terraform -chdir=/terraform output --json nodes | jq -r .[$NUM_NODO].name) ;;
+haproxy) DIRECCION=$(terraform -chdir=/terraform output --json haproxy | jq -r .name) ;;
 esac
 
 printf "\n"
 
-ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+ssh -o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22 -W %h:%p -q root@$HAPROXY_IP" \
+    -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
     -g -L '*:9999:127.0.0.1:32000' -N -f -l root "$DIRECCION"
 
 read -n 1 -s -r -p "Pulsa cualquier tecla para desconectar... "
--- docker/run_on.sh
+++ docker/run_on.sh
@@ -3,10 +3,14 @@
 TIPO_NODO=$(echo "$1" | cut -d "-" -f 2)
 NUM_NODO=$(echo "$1" | cut -d "-" -f 3)
 
+HAPROXY_IP=$(terraform -chdir=/terraform output --json haproxy | jq -r .connection_ip)
+
 case $TIPO_NODO in
-master) DIRECCION=$(terraform -chdir=/terraform output --json master | jq -r .connection_ip) ;;
-node) DIRECCION=$(terraform -chdir=/terraform output --json nodes | jq -r .[$NUM_NODO].connection_ip) ;;
-haproxy) DIRECCION=$(terraform -chdir=/terraform output --json haproxy | jq -r .connection_ip) ;;
+master) DIRECCION=$(terraform -chdir=/terraform output --json master | jq -r .name) ;;
+node) DIRECCION=$(terraform -chdir=/terraform output --json nodes | jq -r .[$NUM_NODO].name) ;;
+haproxy) DIRECCION=$(terraform -chdir=/terraform output --json haproxy | jq -r .name) ;;
 esac
 
-ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$DIRECCION" "$2" && echo ""
+
+ssh -o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22 -W %h:%p -q root@$HAPROXY_IP" \
+-q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$DIRECCION" "$2" && echo ""
