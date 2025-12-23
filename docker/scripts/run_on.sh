65c588b010e5e4b6426b722429a3ebd061065122
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-12-23 12:32:55 +0100

    Usar directamente la dirección del proxy

--- docker/scripts/run_on.sh
+++ docker/scripts/run_on.sh
@@ -8,11 +8,10 @@ HAPROXY_IP=$(terraform -chdir=/terraform output --json haproxy | jq -r .connecti
 case $TIPO_NODO in
 master) DIRECCION=$(terraform -chdir=/terraform output --json master | jq -r .name) ;;
 node) DIRECCION=$(terraform -chdir=/terraform output --json nodes | jq -r .[$NUM_NODO].name) ;;
-haproxy) DIRECCION=$(terraform -chdir=/terraform output --json haproxy | jq -r .name) ;;
 esac
 
 if [ "$TIPO_NODO" = "haproxy" ]; then
-    ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$DIRECCION" "$2" && echo ""
+    ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$HAPROXY_IP" "$2" && echo ""
 else
     ssh -o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22 -W %h:%p -q root@$HAPROXY_IP" \
         -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$DIRECCION" "$2" && echo ""
