c666a65a516d402750432c4363a1b6c8b4ca863d
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-12-23 11:15:15 +0100

    Usar siempre la conexión vía proxy

--- docker/scripts/run_on.sh
+++ docker/scripts/run_on.sh
@@ -4,7 +4,6 @@ TIPO_NODO=$(echo "$1" | cut -d "-" -f 2)
 NUM_NODO=$(echo "$1" | cut -d "-" -f 3)
 
 HAPROXY_IP=$(terraform -chdir=/terraform output --json haproxy | jq -r .connection_ip)
-SSH_PROXY=$(echo local.ansible.connect_to_public_ip | terraform -chdir=/terraform console)
 
 case $TIPO_NODO in
 master) DIRECCION=$(terraform -chdir=/terraform output --json master | jq -r .name) ;;
@@ -12,9 +11,9 @@ node) DIRECCION=$(terraform -chdir=/terraform output --json nodes | jq -r .[$NUM
 haproxy) DIRECCION=$(terraform -chdir=/terraform output --json haproxy | jq -r .name) ;;
 esac
 
-if [ "$SSH_PROXY" = "true" ]; then
+if [ "$TIPO_NODO" = "haproxy" ]; then
+    ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$DIRECCION" "$2" && echo ""
+else
     ssh -o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22 -W %h:%p -q root@$HAPROXY_IP" \
         -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$DIRECCION" "$2" && echo ""
-else
-    ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$DIRECCION" "$2" && echo ""
 fi
